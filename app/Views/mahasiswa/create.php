f89c419cf989e341e8482561af2836d1d41f7eee
kaniarsazr <83527250+kaniarsazr@users.noreply.github.com>
2022-09-13 23:26:54 +0700

    Prak 13 Sept

---
+++ app/Views/mahasiswa/create.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <form>
+        <div class="form-group">
+            <label for="npm">NPM</label>
+            <input type="text" class="form-control" id="npm" name="npm" placeholder="Input NPM. Cth: 2057051014">
+        </div>
+        <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama" placeholder="Input Nama. Cth: Kania Rsazr">
+        </div>
+        <div class="form-group">
+            <label for="alamat">Alamat</label>
+            <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Input Alamat. Ex: Balaraja Tangerang">
+        </div>
+        <button type="submit" class="btn btn-primary">Tambah</button>
+    </form>
+</div>
