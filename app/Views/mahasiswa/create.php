1ebbce10c50614466f435198f630e38c7f308823
kaniarsazr <83527250+kaniarsazr@users.noreply.github.com>
2022-09-21 00:08:26 +0700

    Prak 4 (20/9)

--- app/Views/mahasiswa/create.php
+++ app/Views/mahasiswa/create.php
@@ -1,16 +1,16 @@
 <div class="container">
-    <form>
+    <form action="/store" method="post">
         <div class="form-group">
             <label for="npm">NPM</label>
-            <input type="text" class="form-control" id="npm" name="npm" placeholder="Input NPM. Cth: 2057051014">
+            <input type="text" class="form-control" id="npm" name="npm" placeholder="Input NPM Anda. Ex: 2017051019">
         </div>
         <div class="form-group">
             <label for="nama">Nama</label>
-            <input type="text" class="form-control" id="nama" name="nama" placeholder="Input Nama. Cth: Kania Rsazr">
+            <input type="text" class="form-control" id="nama" name="nama" placeholder="Input Nama Anda. Ex: Rifan Setiadi">
         </div>
         <div class="form-group">
             <label for="alamat">Alamat</label>
-            <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Input Alamat. Ex: Balaraja Tangerang">
+            <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Input alamat Anda. Ex: Tangerang">
         </div>
         <button type="submit" class="btn btn-primary">Tambah</button>
     </form>
